587da3edb2649a6b7547edacb7517d038c14236d
Marcin8811 <tkoz8811@icloud.com>
2024-05-01 19:22:59 +0200

    add function new version

--- funkcje.py
+++ funkcje.py
@@ -1,8 +1,18 @@
-def witaj():
-    print("Witaj Świecie!")
+def witaj(imie):
+    print(f"Witaj {imie}!")
 
-def idz_umyj_rece():
+def idz_umyj_rece(jak_brudne):
     print("Idę umyć ręce!")
+    if jak_brudne =='czyste':
+        print('Po co przychodzisz do łazienki jak masz czyste ręce?')
+    elif jak_brudne == 'lekko':
+        print('Myję ręce mydłem zapachowym')
+    elif jak_brudne == 'bardzo':
+        print('Myję ręce pastą bhp!')
+    else:
+        print("Sprecyzuj jak brudne masz ręce!")
 
-idz_umyj_rece()
-witaj()
+#witaj(input("Podaj swoje imie: "))
+
+idz_umyj_rece(input("Jak brudne masz ręce? (czyste/lekko/bardzo): "))
+
