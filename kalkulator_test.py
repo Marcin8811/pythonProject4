0e8c926e6ca4054c066c81ed3dd122dc6cd87f40
Marcin8811 <tkoz8811@icloud.com>
2024-05-03 12:22:37 +0200

    add klass 3

---
+++ kalkulator_test.py
@@ -0,0 +1,16 @@
+import unittest
+from kalkulator import Kalkulator
+
+class TestKalkulator(unittest.TestCase):
+    def setUp(self):
+        self.kalkulator = Kalkulator()
+
+    def test_dodawania(self):
+        wynik = self.kalkulator.dodaj(10, 20)
+        self.assertEqual(wynik, 30)
+
+
+    unittest.main()
+
+
+
