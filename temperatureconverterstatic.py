fc3d629bee2c82c3c126dda18f6a1b8fa5e9d783
Marcin8811 <tkoz8811@icloud.com>
2024-05-19 19:12:48 +0200

    Task 07.05.2024(7)

--- temperatureconverterstatic.py
+++ temperatureconverterstatic.py
@@ -1,24 +1,23 @@
-class TemperatureConverter:
-    @staticmethod
-    def celsius_to_fahrenheit(celsius):
-        return celsius * 9 / 5 + 32
+from temperatureconverter import TemperatureConverter
+import unittest
 
-    @staticmethod
-    def fahrenheit_to_celsius(fahrenheit):
-        return (fahrenheit - 32) * 5 / 9
+class TestTemperatureConverter(unittest.TestCase):
+    def setUp(self):
+        self.converter = TemperatureConverter()
+    def test_celsius_to_fahrenheit(self):
+        self.assertEqual(32, self.converter.celsius_to_fahrenheit(0))
+        self.assertEqual(212, self.converter.celsius_to_fahrenheit(100))
+    def test_fahrenheit_to_celsius(self):
+        self.assertEqual(0, self.converter.fahrenheit_to_celsius(32))
+        self.assertEqual(100, self.converter.fahrenheit_to_celsius(212))
+    def test_celsius_to_kelvin(self):
+        self.assertEqual(0, self.converter.celsius_to_kelvin(-273.15))
+    def test_kelvin_to_celsius(self):
+        self.assertEqual(-273.15, self.converter.kelvin_to_celsius(0))
+    def test_fahrenheit_to_kelvin(self):
+        self.assertAlmostEqual(310.93, self.converter.fahrenheit_to_kelvin(100), places=2)
+    def test_kelvin_to_fahrenheit(self):
+        self.assertAlmostEqual(100, self.converter.kelvin_to_fahrenheit(310.93), places=2)
 
-    @staticmethod
-    def celsius_to_kelvin(celsius):
-        return celsius +273.15
-
-    @staticmethod
-    def kelvin_to_celsius(kelvin):
-        return kelvin - 273.15
-
-    @staticmethod
-    def fahrenheit_to_kelvin(fahrenheit):
-        return (fahrenheit + 459.67) * 5 / 9
-
-    @staticmethod
-    def kelvin_to_fahrenheit(kelvin):
-        return (kelvin - 459.67) * 5 / 9
+if __name__ == '__main__':
+    unittest.main()
