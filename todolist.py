1c659a23b5ebf1eeced7be838cafe94825f6d1c9
Marcin8811 <tkoz8811@icloud.com>
2024-05-19 17:32:24 +0200

    Task 07.05.2024(7)

--- todolist.py
+++ todolist.py
@@ -4,7 +4,9 @@ class TodoList:
         self.tasks = []
         self.current_id = 0
     def add_task(self, task):
-        pass
+        self.current_id += 1
+        self.tasks.append({'id' : self.current_id, 'task' : task, 'done' : False})
+        return self.current_id
     def remove_task(self, task_id):
         pass
     def mark_task_done(self, task_id):
@@ -14,6 +16,10 @@ class TodoList:
 
 if __name__ == "__main__":
     todolist = TodoList()
+    print(todolist.get_all_tasks())
+    todolist.add_task("Zadanie testowe")
+    print(todolist.get_all_tasks())
+    todolist.add_task("Inne zadanie")
     print(todolist.get_all_tasks())
 
 
