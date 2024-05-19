91ae19f2f4e5c639cff63ef4cc68ca316718b556
Marcin8811 <tkoz8811@icloud.com>
2024-05-19 19:10:16 +0200

    Task 07.05.2024(7)

--- todolist.py
+++ todolist.py
@@ -1,7 +1,3 @@
-#Utwórz klasę TodoList, która pozwoli na zarządzanie listą do wykonania. Napisz testy jednostkowe.
-from asyncio import tasks
-
-
 class TodoList:
     def __init__(self):
         self.tasks = []
@@ -16,33 +12,30 @@ class TodoList:
             if task['id'] != task_id:
                 new_tasks.append(task)
         self.tasks = new_tasks
+
     def mark_task_as_done(self, task_id):
 
         #self.tasks[task_id-1]['done'] = True
-        #new_tasks = []
+        # new_tasks = []
         for task in self.tasks:
             if task['id'] == task_id:
                 task['done'] = True
                 break
             #new_tasks.append(task)
         #self.tasks = new_tasks
-
     def get_all_tasks(self):
         return self.tasks
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     todolist = TodoList()
     #print(todolist.get_all_tasks())
     todolist.add_task("Zadanie testowe")
     #print(todolist.get_all_tasks())
     todolist.add_task("Inne zadanie")
-    todolist.add_task("Trzecie zadanie")
+    todolist.add_task("Trzecie zadanie zadanie")
     #print(todolist.get_all_tasks())
     todolist.remove_task(1)
     print(todolist.get_all_tasks())
     todolist.mark_task_as_done(2)
     print(todolist.get_all_tasks())
-
-
-
-
