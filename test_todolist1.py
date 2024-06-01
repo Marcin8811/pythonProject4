5524c11cef25fb983256d4f3dc7d0996640a47ee
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-06-01 15:18:33 +0200

    Task 07.05.2024(7)

--- test_todolist1.py
+++ test_todolist1.py
@@ -4,28 +4,33 @@ from todolist import TodoList
 class TestTodoList(unittest.TestCase):
     def setUp(self):
         self.todolist = TodoList()
+
     def test_add_task(self):
         self.assertEqual(0, len(self.todolist.get_all_tasks()))
         test_id = self.todolist.add_task("Zadanie testowe")
         self.assertEqual(1, len(self.todolist.get_all_tasks()))
-        self.assertEqual({'id': 1, 'task' : "Zadanie testowe", 'done' : False}, self.todolist.get_all_tasks()[0])
+        self.assertEqual({'id': test_id, 'task': "Zadanie testowe", 'done': False}, self.todolist.get_all_tasks()[0])
 
-    def test_remove_task(self, test_id=1):
+    def test_remove_task(self):
         self.assertEqual(0, len(self.todolist.get_all_tasks()))
-        self.todolist.add_task("Zadanie testowe")
+        test_id = self.todolist.add_task("Zadanie testowe")
         self.assertEqual(1, len(self.todolist.get_all_tasks()))
         self.todolist.remove_task(test_id)
         self.assertEqual(0, len(self.todolist.get_all_tasks()))
 
-    def test_mark_task_as_done(self, test_id=1):
+    def test_mark_task_as_done(self):
         self.assertEqual(0, len(self.todolist.get_all_tasks()))
-        self.todolist.add_task("Zadanie testowe")
+        test_id = self.todolist.add_task("Zadanie testowe")
         self.assertEqual(1, len(self.todolist.get_all_tasks()))
-        self.assertFalse(self.todolist.get_all_tasks()[0]['done'])
+        self.assertFalse(self.todolist.get_all_tasks()[-1]['done'])
         self.todolist.mark_task_as_done(test_id)
-        #self.assertEqual({'id': 1, 'task': "Zadanie testowe", 'done': True}, self.todolist.get_all_tasks()[0])
-        #self.assertEqual(True, self.todolist.get_all_tasks()[0]['done'])
-        self.assertTrue(self.todolist.get_all_tasks()[0]['done'])
+        self.assertTrue(self.todolist.get_all_tasks()[-1]['done'])
+
+    def test_get_all_tasks(self):
+        self.assertEqual(0, len(self.todolist.get_all_tasks()))
+        self.todolist.add_task("Zadanie testowe 1")
+        self.todolist.add_task("Zadanie testowe 2")
+        self.assertEqual(2, len(self.todolist.get_all_tasks()))
 
 if __name__ == "__main__":
     unittest.main()
