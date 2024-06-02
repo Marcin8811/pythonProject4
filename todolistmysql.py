e2fce4a0696d5ad75129cbd4fae4e950fede2cd5
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-06-02 16:51:55 +0200

    Task 07.05.2024(7)

--- todolistmysql.py
+++ todolistmysql.py
@@ -3,29 +3,43 @@ import pymysql
 class TodoList:
     def __init__(self, db):
         self.db = db
-        self.cursor = self.db.cursor()
-        #self.tasks = []
-        #self.current_id = 0
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+
     def add_task(self, task):
-        self.current_id += 1
-        self.tasks.append({'id' : self.current_id, 'task' : task, 'done' : False})
-        return self.current_id
+        sql = "INSERT INTO tasks (task, done) VALUES (%s, %s)"
+        self.cursor.execute(sql, (task, False))
+        self.db.commit()
+        return self.cursor.lastrowid
+
     def remove_task(self, task_id):
-        new_tasks = []
-        for task in self.tasks:
-            if task['id'] != task_id:
-                new_tasks.append(task)
-        self.tasks = new_tasks
+        sql = "DELETE FROM tasks WHERE id = %s"
+        self.cursor.execute(sql, (task_id))
+        self.db.commit()
 
     def mark_task_as_done(self, task_id):
-        for task in self.tasks:
-            if task['id'] == task_id:
-                task['done'] = True
-                break
+        sql = "UPDATE tasks SET done = TRUE WHERE id = %s"
+        self.cursor.execute(sql, (task_id))
+        self.db.commit()
+
     def get_all_tasks(self):
-        return self.tasks
+        sql = "SELECT * FROM tasks"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 if __name__ == '__main__':
     db = pymysql.connect(host='localhost', user='root', password='', db='todolist')
     todolist = TodoList(db)
+
+    print(todolist.get_all_tasks())
+    task_id = todolist.add_task("Nauka kasowania")
+    print(todolist.get_all_tasks())
+    todolist.mark_task_as_done(task_id)
+    print(todolist.get_all_tasks())
+    todolist.remove_task(task_id)
+    print(todolist.get_all_tasks())
+    db.close()
+
+
+
+# CREATE TABLE tasks (id INT NOT NULL AUTO_INCREMENT , task TEXT NOT NULL , done BOOLEAN NOT NULL DEFAULT FALSE , PRIMARY KEY (`id`))
