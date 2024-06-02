1f9acfe6c3084781bd6feca41ee672d42d0de8e7
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-06-02 14:02:01 +0200

    Task 07.05.2024(7)

---
+++ todolistmysql.py
@@ -0,0 +1,28 @@
+import pymysql
+
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+        self.current_id = 0
+    def add_task(self, task):
+        self.current_id += 1
+        self.tasks.append({'id' : self.current_id, 'task' : task, 'done' : False})
+        return self.current_id
+    def remove_task(self, task_id):
+        new_tasks = []
+        for task in self.tasks:
+            if task['id'] != task_id:
+                new_tasks.append(task)
+        self.tasks = new_tasks
+
+    def mark_task_as_done(self, task_id):
+        for task in self.tasks:
+            if task['id'] == task_id:
+                task['done'] = True
+                break
+    def get_all_tasks(self):
+        return self.tasks
+
+
+if __name__ == '__main__':
+    todolist = TodoList()
